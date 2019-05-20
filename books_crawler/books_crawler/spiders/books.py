cc4e5216106174aea534b9401fb927bb743ec93d
ShiviAgrawal113114 <agrawalshivi113114@gmail.com>
2019-05-21 03:22:40 +0530

    books_crawler

---
+++ books_crawler/books_crawler/spiders/books.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class BooksSpider(CrawlSpider):
+    name = 'books'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+    rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
+    def parse_page(self, response):
+    	yield{'URL': response.url}
+
+
+
+
+
+
+
+
+   #rules = (Rule(LinkExtractor(deny_domains=('google.com'), allow =('music')), callback='parse_page', follow=True),)
